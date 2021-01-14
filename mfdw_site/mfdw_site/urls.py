ce630653550236c54efc55dab0a90d69d659f9d2
Kent <kmcalvario@outlook.com>
2021-01-14 10:42:22 -0600

    ADD Quote Forms Quote view Detail view User management TODO QR code in detail view

--- mfdw_site/mfdw_site/urls.py
+++ mfdw_site/mfdw_site/urls.py
@@ -1,8 +1,15 @@
+from django.views.generic.base import TemplateView
+from quotes.views import Register
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('testpage', TemplateView.as_view(template_name='pages/page.html')),
     path('admin/', admin.site.urls),
+    path('register/success', TemplateView.as_view(template_name="registration/success.html"), name='register-success'),
+    path('register/', Register.as_view(), name='register'),
     path('quote/', include('quotes.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
 ]
--- mfdw_site/quotes/views.py
+++ mfdw_site/quotes/views.py
@@ -1,20 +1,66 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Quote
 from .forms import QuoteForm
 from pages.models import Page
 
+class Register(CreateView):
+    template_name= 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
+
+class QuoteView(LoginRequiredMixin, DetailView):
+    #model = Quote
+    context_object_name = 'quote'
+
+    def get_queryset(self):
+        return Quote.objects.filter(username=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super(QuoteView, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
+
+class QuoteList(LoginRequiredMixin, ListView):
+    #model = Quote
+    context_object_name = 'all_quotes'
+
+    def get_queryset(self):
+        return Quote.objects.filter(username=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super(QuoteList, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
+
 def quote_req(request):
-    submitted = false
+    submitted = False
     if request.method == 'POST':
         form = QuoteForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/quote/?submitted=True')
-        else:
-            form = QuoteForm()
-            if 'submitted' in request.GET:
-                submitted = True
 
-        return render(request, 'quotes/quote.html', {'form': 'page_list': Page.objects.all(), 'submitted': submitted})
+
+        if form.is_valid():
+            form.save(commit=False)
+            try:
+                quote.username = request.user
+            except Exeption:
+                pass
+            quote.save()
+            return HttpResponseRedirect('/quote/?submitted=True')
+    else:
+        form = QuoteForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'quotes/quote.html', {'form': form, 'page_list': Page.objects.all(), 'submitted': submitted})
